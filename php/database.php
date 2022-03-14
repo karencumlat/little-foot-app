dffe71c5ca69bee9b84720dc4244a9b4208ca33b
karencumlat <karencumlat@gmail.com>
2022-03-13 19:08:16 -0700

    fix: database php file

--- php/database.php
+++ php/database.php
@@ -1,11 +1,11 @@
 <?php
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"))
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 $servername = $url["host"];
 $username = $url["user"];
 $password = $url["pass"];
-$db = substr($url["path"],1)
+$db = substr($url["path"],1);
 
-$active_group = 'default'
+$active_group = 'default';
 $query_builder = TRUE;
 
 // Create connection
